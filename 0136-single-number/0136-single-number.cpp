251c0f31f9cc7203f153afa3241ddff90730d48a
abhishek19274-cyber <abhishek19274@gmail.com>
2026-07-11 12:54:20 +0530

    Time: 0 ms (100%), Space: 20.7 MB (24.16%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int XOR = 0;
+        for(auto ele : nums){
+            XOR^=ele;
+        }
+        return XOR;
+    }
+};
