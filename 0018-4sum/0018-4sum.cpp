709df02c94382ebd67cad4d7e66123d7db2909a7
abhishek19274-cyber <abhishek19274@gmail.com>
2026-07-14 14:03:09 +0530

    Time: 18 ms (84.01%), Space: 17.6 MB (23.68%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> result;
+        sort(nums.begin(),nums.end());
         int n = nums.size();
-        set<vector<int>> ans_set;
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                unordered_set<long long> hash;
-                for(int k = j + 1; k < n; k++){
-                    long long required = (long long)target - nums[i] - nums[j] - nums[k];
-                    if(hash.find(required) != hash.end()){
-                        vector<int> temp = {nums[i], nums[j], nums[k], (int)required};
-                        sort(temp.begin(), temp.end());
-                        ans_set.insert(temp);
+        for (int i = 0; i < n; i++) {
+            if (i > 0 and nums[i] == nums[i - 1])
+                continue;
+            for (int j = i + 1; j < n; j++) {
+                if (j != i + 1 and nums[j] == nums[j - 1])
+                    continue;
+                int k = n - 1;
+                int l = j + 1;
+                while (l < k) {
+                    long long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if (sum < (long long)target) {
+                        l++;
+                    } else if (sum > (long long)target) {
+                        k--;
+                    } else {
+                        vector<int> temp = {nums[i], nums[j], nums[l], nums[k]};
+                        result.push_back(temp);
+                        l++;
+                        k--;
+                        while (l < k and nums[l] == nums[l - 1]) {
+                            l++;
+                        }
+                        while (l < k and nums[k]==nums[k+1]){
+                            k--;
+                        }
                     }
-                    hash.insert(nums[k]);
                 }
             }
         }
-        vector<vector<int>> result(ans_set.begin(), ans_set.end());
         return result;
     }
 };
