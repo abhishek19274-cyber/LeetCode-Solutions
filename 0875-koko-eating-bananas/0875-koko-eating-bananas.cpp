6fc07e13e36dfda1078bb78fc4c4194c5953e82f
abhishek19274-cyber <abhishek19274@gmail.com>
2026-07-17 13:21:19 +0530

    Time: 7 ms (70.44%), Space: 23.1 MB (15.69%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool helperfunction(vector<int>& piles, int h,int mid){
+        long long total_time_taken = 0;
+        for(auto ele : piles){
+            total_time_taken+=(ele + mid-1)/mid;
+        }
+        return total_time_taken <= h;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        const long long  N= 1e9;
+        long long low =1;
+        long long high =N;
+        while(high-low>1){
+            long long mid = low + ((high - low)>>1);
+            if(helperfunction(piles,h,mid) == false){
+                low = mid;
+            }
+            else{
+                high = mid;
+            }
+        }
+        if(helperfunction(piles,h,low)==true)return low;
+        if(helperfunction(piles,h,high)==true)return high;
+        return -1;
+    }
+};
