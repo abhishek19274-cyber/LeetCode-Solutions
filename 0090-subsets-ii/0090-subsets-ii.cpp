0332b11f73c444afcce6e6fe639c5e8a59f42c40
abhishek19274-cyber <abhishek19274@gmail.com>
2026-08-07 21:23:17 +0530

    Time: 0 ms (100%), Space: 10.4 MB (85.24%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,24 +1,23 @@
+
 class Solution {
 public:
-    void solution(int i,vector<int>& nums,vector<int>& temp,set<vector<int>>& res){
-        if(i==nums.size()){
-            res.insert(temp);
-            return;
+    void backtrack(int start, std::vector<int>& nums, std::vector<int>& temp, std::vector<std::vector<int>>& result) {
+        result.push_back(temp);
+
+        for (int i = start; i < nums.size(); i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue;
+
+            temp.push_back(nums[i]);
+            backtrack(i + 1, nums, temp, result);
+            temp.pop_back(); 
         }
-        solution(i+1,nums,temp,res);
-        temp.push_back(nums[i]);
-        solution(i+1,nums,temp,res);
-        temp.pop_back();
     }
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<int> temp;
-        set<vector<int>> res;
-        vector<vector<int>> result;
-        solution(0,nums,temp,res);
-        for(auto &ele : res){
-            result.push_back(ele);
-        }
+    std::vector<std::vector<int>> subsetsWithDup(std::vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        
+        std::vector<std::vector<int>> result;
+        std::vector<int> temp;
+        backtrack(0, nums, temp, result);
         return result;
     }
 };
