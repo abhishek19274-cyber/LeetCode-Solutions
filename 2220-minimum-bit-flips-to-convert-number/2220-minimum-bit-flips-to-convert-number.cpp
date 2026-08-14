1ff2b9ca99db67be33f720d25957a84b82934429
abhishek19274-cyber <abhishek19274@gmail.com>
2026-08-14 15:24:15 +0530

    Time: 0 ms (100%), Space: 8 MB (60.71%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,6 +1,3 @@
 class Solution {
 public:
-    int minBitFlips(int start, int goal) {
-        return __builtin_popcount(start ^ goal);
-    }
-};
+    int minBitFlips(int start, int goal) {return __builtin_popcount(start ^ goal);}};
