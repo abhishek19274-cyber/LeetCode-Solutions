f52af9a0a6df1d0e92cf5004756706fc16568f98
abhishek19274-cyber <abhishek19274@gmail.com>
2026-08-29 13:47:27 +0530

    Time: 0 ms (100%), Space: 10.2 MB (75.32%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int function(vector<int>& nums){
-        int prev =nums[0];
-        int curr = 0;
-        int prev2 = 0;
+        int prev =nums[0],prev2 = 0,curr = 0;
         for(int i=1;i<nums.size();i++){
             int take = nums[i] + ((i > 1) ? prev2 : 0);
             int not_take = 0 + prev;
@@ -15,8 +13,7 @@ public:
     }
     int rob(vector<int>& nums) {
         if(nums.size()==1)return nums[0];
-        vector<int> temp1;
-        vector<int> temp2;
+        vector<int> temp1,temp2;
         for(int i =0;i<nums.size();i++){
             if(i!= 0)temp1.push_back(nums[i]);
             if(i!=nums.size()-1)temp2.push_back(nums[i]);
