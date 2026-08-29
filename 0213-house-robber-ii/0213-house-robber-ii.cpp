78450050852c49a2b63f549a91930007f171b413
abhishek19274-cyber <abhishek19274@gmail.com>
2026-08-29 13:46:53 +0530

    Time: 0 ms (100%), Space: 10.3 MB (75.32%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int function(vector<int>& nums){
+        int prev =nums[0];
+        int curr = 0;
+        int prev2 = 0;
+        for(int i=1;i<nums.size();i++){
+            int take = nums[i] + ((i > 1) ? prev2 : 0);
+            int not_take = 0 + prev;
+            curr = max(take,not_take);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+    int rob(vector<int>& nums) {
+        if(nums.size()==1)return nums[0];
+        vector<int> temp1;
+        vector<int> temp2;
+        for(int i =0;i<nums.size();i++){
+            if(i!= 0)temp1.push_back(nums[i]);
+            if(i!=nums.size()-1)temp2.push_back(nums[i]);
+        }
+        return max(function(temp1),function(temp2));
+    }
+};
